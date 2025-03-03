e39307f66a339951b90e3a21532dd32f135eec9c
martin-kullamaa <martin.kullamaa@gmail.com>
2025-03-03 16:14:21 +0200

    init

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,61 @@
 <template>
   <div>
-    LOGI SISSE
+    <div class="container text-center">
+      <div class="row justify-content-center">
+
+        <div class="col col-3">
+          <div class="input-group mb-3">
+            <input v-model="username" type="text" class="form-control" placeholder="Username">
+          </div>
+          <div class="input-group mb-3">
+            <input v-model="password" type="text" class="form-control" placeholder="Password">
+          </div>
+          <div class="mb-3">
+          <button @click="login" type="button" class="btn btn-info">Log in</button>
+          </div>
+          <div>
+          <button type="button" class="btn btn-info">Register</button>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "LoginView"
+  name: 'LoginView',
+  data() {
+    return {
+      username: '',
+      password: '',
+      loginResponse: {
+        userId: 0,
+        roleName: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      if (this.username.length > 0 && this.password.length > 0) {
+        sendLoginRequest(this.username, this.password)
+        {
+          axios.get('/login', {
+                params: {
+                  username: this.username,
+                  password: this.password
+                }
+              }
+          )
+              .then(response => this.loginResponse = response.data)
+              .catch(error => this.someDataBlockErrorResponseObject = error.response.data)
+        }
+      }
+    }
+  }
 }
 </script>
