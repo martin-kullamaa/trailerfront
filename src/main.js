23e2c013135327c989cb8e1fb7df59988175ace6
martin-kullamaa <martin.kullamaa@gmail.com>
2025-03-03 15:00:14 +0200

    init

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/css/style.css' // kui on oma style.css fail
+
 createApp(App).use(router).mount('#app')
