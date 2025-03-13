60b5b0a57312ee0be8908bc130aa3690fa1be156
opilane <maarja.karu@gmail.com>
2025-03-13 13:32:51 +0200

    trail view ver 2

--- src/views/TrailView.vue
+++ src/views/TrailView.vue
@@ -17,29 +17,51 @@
               <p class="text-start">{{ trail.trailDescription }}</p>
             </div>
 
-          </div>
-        </div>
-        <div class="card transparent-card">
-          <h6 class="mt-2">Added equipment:</h6>
-          <div class="container text-center">
+            <!--            uus div-->
+            <div>
+              <h6 class="mt-2">Added equipment:</h6>
+              <div class="container text-center">
           <span v-for="trailEquipment in trailEquipment" :key="trailEquipment.equipmentId"
                 @click="deleteTrailEquipment(trailEquipment.equipmentId)"
                 class="badge text-bg-success custom-badge pointer me-3">
                 {{ trailEquipment.name }}
           </span>
-          </div>
-          <div class="d-flex align-items-center justify-content-center flex-wrap py-2">
-            <div class="mx-2">
-              <span class="badge text-bg-success custom-badge">Type(s):</span>
+              </div>
+              <div class="d-flex align-items-center justify-content-center flex-wrap py-2">
+                <div class="mx-2">
+                  <span class="badge text-bg-success custom-badge">Type(s):</span>
+                </div>
+                <div v-for="trailType in trailTypes" :key="trailType.typeId" class="mx-2">
+                  <font-awesome-icon :icon="getTypeIcon(trailType.typeId)" class="main-icon small"/>
+                </div>
+                <div class="mx-2">
+                  <span class="badge text-bg-success custom-badge">Trail length: {{ trail.trailLength }}km</span>
+                </div>
+              </div>
             </div>
-            <div v-for="trailType in trailTypes" :key="trailType.typeId" class="mx-2">
-              <font-awesome-icon :icon="getTypeIcon(trailType.typeId)" class="main-icon small"/>
+            <!--            uus div-->
+
+            <div class="card-body">
+              <h6 class="mt-2">Lisatud pildid:</h6>
+              <div v-if="trailPictures.length > 0" class="mt-3">
+                <div class="container">
+                  <div class="row">
+                    <div v-for="(pic, index) in trailPictures" :key="index" class="col-4 mb-3">
+                      <div class="d-flex flex-column align-items-center">
+                        <img :src="pic.data" alt="picture" class="img-thumbnail"
+                             style="width: 100px; height: 100px; object-fit: cover;">
+                        <span class="mt-2 small">{{ pic.name }}</span>
+                        <button @click="removePicture(index)" class="btn btn-sm btn-outline-danger mt-2">Eemalda</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="mx-2">
-              <span class="badge text-bg-success custom-badge">Trail length: {{ trail.trailLength }}km</span>
-            </div>
+
           </div>
         </div>
+
       </div>
 
       <div class="col">
@@ -55,28 +77,6 @@
         </div>
 
 
-
-      </div>
-    </div>
-    <div class="row mt-5">
-      <div class="col-md-12">
-        <div class="card transparent-card mb-3">
-          <div class="card-body">
-            <h6 class="mt-2">Added picture(s):</h6>
-            <!-- Picture content here -->
-            <div v-if="trailPictures.length > 0" class="mt-3">
-              <div class="container">
-                <div v-for="(pic, index) in trailPictures" :key="index"
-                     class="d-flex align-items-center py-2 border-bottom">
-                  <img :src="pic.data" alt="picture" class="img-thumbnail"
-                       style="width: 100px; height: 100px; object-fit: cover;">
-                  <span class="ms-3 flex-grow-1 small">{{ pic.name }}</span>
-                  <button @click="removePicture(index)" class="btn btn-sm btn-outline-danger">Remove</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   </div>
