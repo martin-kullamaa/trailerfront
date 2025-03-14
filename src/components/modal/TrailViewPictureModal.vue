e5eeda1258ad4ff53dce7f67c3126b59493e197e
opilane <maarja.karu@gmail.com>
2025-03-14 16:35:23 +0200

    Modal WIP

--- src/components/modal/TrailViewPictureModal.vue
+++ src/components/modal/TrailViewPictureModal.vue
@@ -1,17 +1,19 @@
 <template>
-  <Modal :modal-is-open="modalIsOpen" :fullScreen="true" @event-close-modal="closeModal">
+  <Modal
+      :modal-is-open="modalIsOpen"
+      :class="{ 'is-fullscreen': fullScreen }"
+      @event-close-modal="closeModal"
+  >
     <template #body>
-      <div class="modal-dialog modal-fullscreen">
-        <div class="modal-content">
+      <div class="modal-container" :class="{ 'is-fullscreen': fullScreen }">
+        <div class="modal-content" :class="{ 'is-fullscreen': fullScreen }">
           <div class="modal-body">
-            <div class="image-wrapper">
-              <img
-                  :src="pic.data"
-                  alt="Pilt"
-                  class="img-fluid"
-                  style="max-width: 100%; max-height: 90vh; object-fit: contain;"
-              />
-            </div>
+            <img
+                :src="pic.data"
+                alt="Pilt"
+                class="img-fluid"
+                style="max-width: 100%; max-height: 100vh; object-fit: contain;"
+            />
           </div>
           <div class="modal-footer">
             <slot name="footer">{{ pic.name }}</slot>
@@ -33,33 +35,14 @@ export default {
       type: Boolean,
       required: true
     },
+    fullScreen: { // Uus prop fullscreen jaoks
+      type: Boolean,
+      default: false
+    },
     pic: {
       type: Object,
       required: true
-    },
+    }
   }
 }
 </script>
-
-<style scoped>
-.modal-body {
-  display: flex;
-  justify-content: center; /* Horisontaalselt keskele */
-  align-items: center; /* Vertikaalselt keskele */
-  height: 80vh; /* Tagab, et keha täidab suure osa modali kõrgusest */
-}
-
-.image-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-}
-
-.modal-footer {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
